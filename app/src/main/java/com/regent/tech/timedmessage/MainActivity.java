92ebdc61ef415a6ab49021b2f36b3296fd5bfe71
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-27 10:01:59 +0100

    Setup FloatingActionButton

--- app/src/main/java/com/regent/tech/timedmessage/MainActivity.java
+++ app/src/main/java/com/regent/tech/timedmessage/MainActivity.java
@@ -1,13 +1,32 @@
 package com.regent.tech.timedmessage;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton actionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionButton = (FloatingActionButton) findViewById(R.id.fab);
+        setupFloatingActionButton();
+
+    }
+
+    private void setupFloatingActionButton(){
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewMessageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
