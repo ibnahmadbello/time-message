fb8cf447174be2fb901c669002815d0b52277a32
Ibn Ahmad Bello <justforlearningcode@gmail.com>
2018-02-28 21:10:13 +0100

    Declared and Casted various views in the NewMessageActivity

--- app/src/main/java/com/regent/tech/timedmessage/MainActivity.java
+++ app/src/main/java/com/regent/tech/timedmessage/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/regent/tech/timedmessage/NewMessageActivity.java
+++ app/src/main/java/com/regent/tech/timedmessage/NewMessageActivity.java
@@ -2,12 +2,25 @@ package com.regent.tech.timedmessage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewMessageActivity extends AppCompatActivity {
+
+    private EditText mPhoneNumber;
+    private EditText mTextMessage;
+    private Button mSendNow;
+    private Button mSendLater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+
+        mPhoneNumber = (EditText) findViewById(R.id.phone_number);
+        mTextMessage = (EditText) findViewById(R.id.text_message);
+        mSendNow = (Button) findViewById(R.id.send_now);
+        mSendLater = (Button) findViewById(R.id.send_later);
+
     }
 }
